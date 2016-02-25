15cbb97d9261ab038d30965631bdea715a8258e0
anjalyj <anjalyj@thoughtworks.com>
2016-02-25 14:30:43 +0530

    Added functionality to total the count when more than 1 file is given.

--- src/Operations.java
+++ src/Operations.java
@@ -4,18 +4,31 @@
 public class Operations {
     private String[] files;
     private String[] options;
-    public Operations(String[] files,String[] options){
+
+    public Operations(String[] files, String[] options) {
         this.files = files;
         this.options = options;
     }
 
-    public void wcWithoutOption(){
-        for(int i=0; i< files.length; i++){
+    public void wcWithoutOption() {
+        int total[] = {0, 0, 0};
+        for (int i = 0; i < files.length; i++) {
             int result[] = new int[3];
             Reader fr = new Reader(files[i]);
             WordCount wc = new WordCount(fr.readContent());
             result = wc.l_w_c_Count();
-            System.out.println(" "+result[0]+"	"+result[1]+"	"+result[2]+ " "+files[i]);
+              total =  getTotal(result, total);
+            System.out.println(" " + result[0] + "	" + result[1] + "	" + result[2] + " " + files[i]);
         }
+        if (files[1] != null)
+            System.out.println(" " + total[0] + "	" + total[1] + "	" + total[2] + " total");
     }
+
+    private int[] getTotal(int[] values, int[] result) {
+        int index = 0;
+        for (int value : values)
+            result[index++] += value;
+        return result;
+    }
+
 }
